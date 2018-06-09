04b25ac4b41c57077661f04ae194d83cdeb41846
Bobby Martin <robert.martin.619@gmail.com>
2018-06-08 18:36:25 -0700

    Add scrolled class for nav and same functions

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 // import $ from 'jquery';
 import 'bootstrap';
 import './styles/styles.scss';
+
+let scrollPos = 0;
+const navBar = document.getElementById('nav');
+function navbarScrolled() {
+  navBar.classList.add('scrolled');
+}
+function navBarNotScrolled() {
+  navBar.classList.remove('scrolled');
+}
+window.addEventListener('scroll', () => {
+  scrollPos = window.scrollY;
+  if(scrollPos > 10) {
+    navbarScrolled();
+  } else if (scrollPos === 0) {
+    navBarNotScrolled();
+  }
+});
