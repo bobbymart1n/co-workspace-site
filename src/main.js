16c592f7a2cdd96d2c8f370728abf4fbcd46f228
Bobby Martin <robert.martin.619@gmail.com>
2018-06-08 18:46:10 -0700

    Add further refactor

--- src/main.js
+++ src/main.js
@@ -4,12 +4,9 @@ import './styles/styles.scss';
 
 let scrollPos = 0;
 const navBar = document.getElementById('nav');
-function navbarScrolled() {
-  navBar.classList.add('scrolled');
-}
-function navBarNotScrolled() {
-  navBar.classList.remove('scrolled');
-}
+const navbarScrolled = () => navBar.classList.add('scrolled');
+const navBarNotScrolled = () => navBar.classList.remove('scrolled');
+
 window.addEventListener('scroll', () => {
   scrollPos = window.scrollY;
   scrollPos > 10 ? navbarScrolled() : navBarNotScrolled();
